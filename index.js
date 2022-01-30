ada0d0b026ae72d4d38f222f5797cdb9f5b7bb7b
Wojciech <szmelczerczykwojciech@gmail.com>
2022-01-30 12:09:06 +0100

    delete book from library functionality added

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ app.listen(port, () => console.log("Server listening on port 3001"));
 
 app.get("/books", (req, res) => {
   const books = require("./db/books.json");
+
   res.json(books);
 });
 
 app.get("/books/:id", (req, res) => {
   const id = parseInt(req.params.id);
+
   let selectedBook;
 
   fs.readFile(`${process.cwd()}${dbPath}`, "utf-8", (err, data) => {
     if (err) console.log(err);
     else {
       let obj = JSON.parse(data); // parse JSON to object
+
       selectedBook = obj.filter((book) => book["id"] === id); // return specific book
+
       res.json(selectedBook);
     }
   });
@@ -38,8 +42,11 @@ app.post("/books/create", (req, res) => {
       console.log(err);
     } else {
       let obj = JSON.parse(data); //now converting it to an object
+
       obj.push(book); //adding the data
+
       let json = JSON.stringify(obj, null, 2); //converting it back to json
+
       fs.writeFile(`${process.cwd()}${dbPath}`, json, "utf8", (err) => {
         if (err) {
           console.log(err);
@@ -51,6 +58,28 @@ app.post("/books/create", (req, res) => {
   });
 });
 
-app.delete("/books/:id", (req, res) => {});
+app.delete("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  fs.readFile(`${process.cwd()}${dbPath}`, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let obj = JSON.parse(data); //now converting it to an object
+
+      let deletedBook = obj.filter((book) => book["id"] !== id); // return array without element with id passed as a parameter
+
+      let json = JSON.stringify(deletedBook, null, 2); //converting it back to json
+
+      fs.writeFile(`${process.cwd()}${dbPath}`, json, "utf8", (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Book deleted from library!");
+        }
+      });
+    }
+  });
+});
 
 app.put("/books/:id", (req, res) => {});
